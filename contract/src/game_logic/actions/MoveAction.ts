d3a76b2e64503177c8de704b0cdc710b82c6a5d7
1KitCat1 <denischool119@gmail.com>
2023-08-30 13:20:39 +0300

    Add Move Action

---
+++ contract/src/game_logic/actions/MoveAction.ts
@@ -0,0 +1,78 @@
+import {Action} from "./Action";
+import {Board} from "../Board";
+import {getPlayerById} from "../Utils";
+import {CommunityChestTile} from "../Tiles/CommunityChestTile";
+import {ChanceTile} from "../Tiles/ChanceTile";
+import {TaxTile} from "../Tiles/TaxTile";
+import {JailTile} from "../Tiles/JailTile";
+import {GoToJailAction} from "./GoToJailAction";
+
+export class MoveAction extends Action {
+  public readonly position: number;
+
+  constructor(playerId: string, position: number, dice: Array<number>) {
+    super(dice, playerId);
+
+    this.position = position;
+  }
+
+  public doAction(board: Board): Array<Action> {
+    if (this.dice.length == 0) {
+       board.advancePlayer(this.position, this.playerId);
+       return [];
+    }
+
+    const actions = board.movePlayerToNewTile(this.dice, this.playerId);
+
+    const player = getPlayerById(this.playerId, board.players);
+    const playerPosition = player.getPosition();
+    const currTile = board.tiles[playerPosition];
+
+    if (currTile instanceof CommunityChestTile || currTile instanceof ChanceTile) {
+      actions.push(currTile.doTask(this.playerId, board.players));
+    } else if (currTile instanceof TaxTile) {
+      actions.push(currTile.getPayTaxAction(this.playerId));
+    } else if (currTile instanceof JailTile) {
+      actions.push(new GoToJailAction(this.dice, this.playerId));
+    } else if (currTile.hasOwner() && currTile.getOwnerId() != this.playerId) {
+      actions.push(currTile.getPayRentAction(this.playerId, this.dice));
+    } else if (playerPosition == 24) {
+      actions.push(new GoToJailAction(this.dice, this.playerId));
+    }
+
+    actions.forEach((action) => {
+      action.doAction(board);
+    });
+
+    let result = new Array<Action> (this);
+    result.push(...actions);
+
+    this.generateHistoryMessage(board);
+
+    return result;
+  }
+
+  public getNewPosition() {
+    let result = this.position;
+    if (this.dice.length != 0) {
+      result += this.dice[0] + this.dice[1];
+      if (result > 31) {
+        result -= 32;
+      }
+    }
+
+    return result;
+  }
+
+  private generateHistoryMessage(board: Board) {
+    if (this.dice.length == 0) return;
+
+    const player = getPlayerById(this.playerId, board.players);
+
+    this.historyMessage = "(" + player.color + ").{" + player.id + "} rolled the dice " + this.dice[0] + " and " + this.dice[1];
+  }
+
+  public getHistoryMessage(): string {
+    return this.historyMessage;
+  }
+}
